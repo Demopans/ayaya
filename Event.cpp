72418971564b95738db20dc6a4f78a559d4c4a5c
Demopans <gao.jeff3@gmail.com>
2021-07-19 23:34:09 -0400

    first

---
+++ Event.cpp
@@ -0,0 +1,5 @@
+//
+// Created by demo on 7/19/2021.
+//
+
+#include "Event.h"
---
+++ Event.h
@@ -0,0 +1,14 @@
+//
+// Created by demo on 7/19/2021.
+//
+
+#ifndef PROJECT_EVENT_H
+#define PROJECT_EVENT_H
+
+
+class Event {
+
+};
+
+
+#endif //PROJECT_EVENT_H
